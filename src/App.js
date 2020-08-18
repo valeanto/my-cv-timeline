fa8f294492afb94489821db22f58f39ec5a3ee6d
Valentina Antollini <>
2020-08-18 15:56:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Timeline from './components/Timeline';
+import './App.css';
+
+const App =()=> (
+  <>
+      <Timeline />
+  </>
+
+);
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const data = [
+    {
+        title: 'A-Z',
+        description: 'UK building regulations must be updated to clearly signpost how and when it will transition to mandatory verification of in-use energy consumption. Building industry to adopt Energy Use Intensity (EUI) targets. ',
+
+        published: 'March 03 2017',
+
+        category: {
+            tag: 'course',            
+            subject: 'data-science',
+            color: '#018f69'
+        },
+        link: {
+            url:
+                'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+            text: 'Read more'
+        }
+        
+    },
+    {
+        title: 'Architecture',
+        text: '2030',
+        description: 'UK building regulations must be updated to clearly signpost how and when it will transition to mandatory verification of in-use energy consumption. Building industry to adopt Energy Use Intensity (EUI) targets. ',
+
+        published: 'March 03 2017',
+
+        category: {
+            tag: 'book',            
+            subject: 'architecture',
+            color: '#f1f1f1'
+        },
+        link: {
+            url:
+                'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+            text: 'Read more'
+        }
+    }
+];
+
+export default data;
